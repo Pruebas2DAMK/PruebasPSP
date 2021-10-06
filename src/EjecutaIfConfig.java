3444a6447c6c5c4cc500297e05f0c99617fbc054
JoelS <joelsempere96@gmail.com>
2021-10-06 16:29:23 +0200

    Modificado el ifconfig para añadir 'x' comando

--- src/EjecutaIfConfig.java
+++ src/EjecutaIfConfig.java
@@ -1,11 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class EjecutaIfConfig {
     public static void main(String[] args) throws IOException {
+        //Al final lo he puesto para cualquier comando con un Scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Dame un comando a ejecutar");
+        String EntradaUser = sc.nextLine();
+
         String linea;
-        Process p = Runtime.getRuntime().exec("ifconfig");
+        Process p = Runtime.getRuntime().exec(EntradaUser);
         try(BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))){
             while ((linea = br.readLine()) != null) {
                 System.out.println(linea);
