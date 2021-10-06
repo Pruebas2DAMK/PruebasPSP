9a813e227ca43ec8ff781601821378440b57da6c
JoelS <joelsempere96@gmail.com>
2021-10-06 16:25:38 +0200

    Probando procesos, añadido ejercicio ifconfig

--- src/AbrirCalculadoraProceso.java
+++ src/AbrirCalculadoraProceso.java
@@ -1,4 +1,4 @@
-public class abrirCalculadoraProceso {
+public class AbrirCalculadoraProceso {
     public static void main(String[] args) {
         if (args.length <= 0){
             System.err.println("Se necesita un argumento a ejecutar");
---
+++ src/EjecutaIfConfig.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class EjecutaIfConfig {
+    public static void main(String[] args) throws IOException {
+        String linea;
+        Process p = Runtime.getRuntime().exec("ifconfig");
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()))){
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
