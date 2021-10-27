54e16ca9aca81682c5faf3a846a21aec07296fdc
JoelS <joelsempere96@gmail.com>
2021-10-27 15:51:16 +0200

    Ejercicio Fibonacci hecho

--- src/thread/Fibonacci.java
+++ src/thread/Fibonacci.java
@@ -3,22 +3,27 @@ package thread;
 import java.util.Scanner;
 
 public class Fibonacci extends Thread{
+    @Override
     public void run(){
         Scanner sc = new Scanner(System.in);
         System.out.println("¿Cuantas veces me voy a repetir?");
         int numeroRepeticiones= sc.nextInt();
+        System.out.println(calculaNumeros(numeroRepeticiones));
+    }
+
+    private String calculaNumeros(int n) {
         int ultimoNumero=1;
         int penultimoNumero=1;
         String resultado="";
         resultado+=ultimoNumero+","+penultimoNumero+",";
-        for (int i = 1; i <=numeroRepeticiones;i++){
+        for (int i = 1; i <= n; i++){
             int suma=ultimoNumero+penultimoNumero;
             resultado+=suma+",";
             penultimoNumero=ultimoNumero;
             ultimoNumero=suma;
         }
         resultado=resultado.substring(0,resultado.length()-1);
-        System.out.println(resultado);
+        return resultado;
     }
 
 }
