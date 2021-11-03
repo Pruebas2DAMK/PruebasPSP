198bcff5f8890c36be1daf13e164ae09c067ccf4
JoelS <joelsempere96@gmail.com>
2021-11-03 17:07:22 +0100

    hilos terminar interrupcion

--- src/thread/MuestraNumsConRetardo.java
+++ src/thread/MuestraNumsConRetardo.java
@@ -2,26 +2,35 @@ package thread;
 
 public class MuestraNumsConRetardo implements Runnable{
     Thread t;
-    public MuestraNumsConRetardo() {
+    String nombre;
+    public MuestraNumsConRetardo(String nombre) {
         t = new Thread(this);
+        this.nombre=nombre;
         t.start();
+    }
+    public void Interrumpir(){
+        t.interrupt();
     }
 
     @Override
     public void run(){
-        for (int i=1;i<=20;i++){
-            System.out.println(i);
-            try {
-                Thread.sleep(1500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        if (!t.isInterrupted()) {
+            for (int i = 1; i <= 20; i++) {
+                System.out.println(nombre+" "+i);
+                try {
+                    Thread.sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
 
+            }
         }
     }
 
     public static void main(String[] args) {
-        MuestraNumsConRetardo mncr = new MuestraNumsConRetardo();
+        MuestraNumsConRetardo mncr = new MuestraNumsConRetardo("pepe");
+        mncr.Interrumpir();
+        MuestraNumsConRetardo mncr2 = new MuestraNumsConRetardo("manolo");
     }
 
 }
